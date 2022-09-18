6ad9a0178ad293212b0305955ad911d2545b6ddd
Ahmed Muse <ahmedhared63@gmail.com>
2022-09-17 23:45:37 -0700

    add files

---
+++ README.md
@@ -0,0 +1,42 @@
+# E-Commerce_Back_End_ORM
+
+**What does this application do and why**
+
+* As an manager of an internet retail company, it's important to use good technologies that track what will be available in our store using a database
+* This app builds the routes on the back end to access data from a database or change the data in some way: creating new items for the database, updating items in the database, and deleting things from the database 
+* This application uses different types of api requests to access and change a given database, which will be a good basis for the future website's front-end
+
+**How the application is organized**
+
+E-Commerce Back End ORM is organized into the following files:
+
+![Files](./assets/Files.png)
+
+**Installation**
+
+* open integrated terminal from server.js
+* install necessary applications by using "npm i" in the command line
+* run mySQL and upload the schema.sql file 
+* exit mySQl and run the seeds/index.js to seed the database
+
+**Step by step on how to run the application**
+
+* from the integrated terminal, start the server on the local host
+* use third party application to test different api routes to get/manipulate the data from the database
+
+**What the application looks like**
+
+Here is a video showing the use of the application
+[Link to Video of Application]https://watch.screencastify.com/v/n8x11lR0wjUYLJG0bdJ8
+
+**Link to deployed version of Ahmed's Portfolio**
+
+GitHub Repository: 
+
+**Technologies Used** 
+* javascript
+* Express
+* inquirer
+* MySQL
+* Sequelize
+* Api routes
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,99 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll(
+      {include: [{
+        model:Product,
+        through:ProductTag
+      }]
+      }
+    );
+    res.status(200).json(tagData);
+  }catch(err){
+    res.status(500).json(err);
+  }
+}
+  // find all tags
+  // be sure to include its associated Product data
+);
+
+//get one tag
+router.get('/:id', async (req, res) => {
+  try{
+    const tagData = await Tag.findOne(
+      {
+        where:{
+          id: req.params.id
+        },
+        include: [
+          {
+            model:Product, 
+            through: ProductTag
+          }
+        ]
+    }
+    );
+    if(!tagData){
+      res.status(404).json({ message: 'No tag found with this id!'});
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+});
+
+//create new tag
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(400).json(err);
+  }
+});
+
+//update tag
+router.put('/:id', async (req, res) => {
+try {
+  const tagData = await Tag.update(req.body,
+    {
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.status(200).json(tagData)
+} catch (err){
+  res.status(500).json(err);
+}
+});
+
+//delete tag
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    
+    if(!tagData) {
+      res.status(404).json({ message: "No tag found with this id!"});
+      return;
+    }
+
+    res.status(200).json(tagData);
+  }catch(err){
+    res.status(500).json(err);
+  }
+  // delete on tag by its `id` value
+});
+
+module.exports = router;
